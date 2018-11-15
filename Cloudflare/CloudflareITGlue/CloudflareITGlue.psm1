2e4012fdde5f3bd27692c4e62ac114a27bb016c3
Jeremy Colby <jcolby@yournucleus.ca>
2018-11-15 10:49:02 -0800

    Cloudflare Module 1.1.0

--- Cloudflare/CloudflareITGlue/CloudflareITGlue.psm1
+++ Cloudflare/CloudflareITGlue/CloudflareITGlue.psm1
@@ -3,10 +3,9 @@
 if (Test-Path "$ModuleBase\$env:username.auth") {
     Write-Host "CloudflareITGlue: Auth detected for $env:username" -ForegroundColor Green
     $Auth = Import-Csv "$ModuleBase\$env:username.auth"
-    
-    Set-Variable -Name CloudflareAPIEmail -Scope global -Value $Auth.CloudflareEmail
-    Set-Variable -Name CloudflareAPIKey -Scope global -Value ($Auth.CloudflareAPIKey | ConvertTo-SecureString)
-    Set-Variable -Name ITGlueAPIKey -Scope global -Value ($Auth.ITGlueAPIKey | ConvertTo-SecureString)
+    $Global:CloudflareAPIEmail = $Auth.CloudflareEmail
+    $Global:CloudflareAPIKey = ($Auth.CloudflareAPIKey | ConvertTo-SecureString)
+    $Global:ITGlueAPIKey = ($Auth.ITGlueAPIKey | ConvertTo-SecureString)
 }
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
